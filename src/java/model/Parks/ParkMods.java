4d09f8c9a7a86e1ee3b53450e8e7d7dcd54b0668
Dan M McGinnis <mcginnis.dan@gmail.com>
2014-03-20 21:35:27 -0400

    parkMods.java created

--- src/java/model/Parks/ParkMods.java
+++ src/java/model/Parks/ParkMods.java
@@ -1,6 +1,7 @@
 package model.Parks;
 
 import SQL.DbConn;
+import SQL.DbEncodeUtils;
 import java.sql.*;
 
 /** 
@@ -76,5 +77,71 @@ public class ParkMods {
             
             return this.errorMsg;
         } // catch
-    }// method delete
+    }// method delete   
+    
+    /* This method requires a pre-validated Other data object. 
+     * It also assumes that an open database connection was provided to the constructor.
+     * It returns true if it is able to insert the user data into the database.
+     */
+    public String insert(Validate parkValidate) {
+
+        this.errorMsg = "";// empty error message means it worked.
+        this.debugMsg = "";
+
+        // dont even try to insert if the user data didnt pass validation.
+        if (!parkValidate.isValidated()) {
+            this.errorMsg = "Please edit record and resubmit before inserting";
+            return this.errorMsg;
+        }
+
+        TypedData parkTypedData = (TypedData) parkValidate.getTypedData();
+        String sql = "INSERT INTO parks (park_name, overnight_fee, state_name,) "
+                + "VALUES (?,?,?,?,?)";
+        try {
+            PreparedStatement pStatement = dbc.getConn().prepareStatement(sql);
+            this.debugMsg += DbEncodeUtils.encodeString(pStatement, 1, parkTypedData.getParkName());
+            this.debugMsg += DbEncodeUtils.encodeDecimal(pStatement, 2, parkTypedData.getOverNightFee());
+            this.debugMsg += DbEncodeUtils.encodeString(pStatement, 3, parkTypedData.getStateName());
+
+            //System.out.println("************* got past encoding");
+            try {
+                int numRows = pStatement.executeUpdate();
+                if (numRows == 1) {
+                    return ""; // all is GOOD, one record inserted is what we expect
+                } else {
+                    this.errorMsg = "Error: " + new Integer(numRows).toString()
+                            + " records were inserted where only 1 expected."; // probably never get here, bulk sql insert
+                    return this.errorMsg;
+                }
+            } // try execute the statement
+            catch (SQLException e) {
+                if (e.getSQLState().equalsIgnoreCase("S1000")) {
+                    // this error would only be possible for a non-auto-increment primary key.
+                    this.errorMsg = "Cannot insert: a record with that ID already exists.";
+                } else if (e.getMessage().toLowerCase().contains("duplicate entry")) {
+                    this.errorMsg = "Cannot insert: duplicate entry."; // for example a unique key constraint.
+                } else if (e.getMessage().toLowerCase().contains("foreign key")) {
+                    this.errorMsg = "Cannot insert: invalid reference (bad foreign key value)."; // for example a unique key constraint.
+                } else {
+                    this.errorMsg = "ParkMods.insert: SQL Exception while attempting insert. "
+                            + "SQLState:" + e.getSQLState()
+                            + ", Error message: " + e.getMessage();
+                    // this message would show up in the NetBeans log window (below the editor)
+                    System.out.println("************* " + this.errorMsg);
+                }
+                return this.errorMsg;
+            } // catch
+            catch (Exception e) {
+                // this message would show up in the NetBeans log window (below the editor)
+                this.errorMsg = "ParkMods.insert: General Error while attempting the insert. " + e.getMessage();
+                System.out.println("****************** " + this.errorMsg);
+                return this.errorMsg;
+            } // catch
+        } // trying to prepare the statement
+        catch (Exception e) {
+            this.errorMsg = "ParkMods.insert: General Error while trying to prepare the SQL INSERT statement. " + e.getMessage();
+            System.out.println("****************** " + this.errorMsg);
+            return this.errorMsg;
+        }
+    }// method
 } // class
